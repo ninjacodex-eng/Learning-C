53192babc04e71536f16da449dc32ac9d3a2e902
Aditya <adityaboura09@gmail.com>
2026-06-11 17:19:36 +0530

    Add even-odd program in C

---
+++ even_odd_program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a=0;
+    printf("Enter a no : ");
+    scanf("%d",&a);
+    if (a%2==0){
+        printf("even no ");
+    }else{
+        printf("odd no ");
+    }
+    return 0;
+}
