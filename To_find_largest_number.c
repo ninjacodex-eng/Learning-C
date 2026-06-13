6e2a52ea9a1db6672578d8fbffa23cc8f13daaa7
Aditya <adityaboura09@gmail.com>
2026-06-13 13:30:56 +0530

    Refactor largest number logic to include equality

--- To_find_largest_number.c
+++ To_find_largest_number.c
@@ -3,12 +3,12 @@ int main(){
     int a,b,c;
     printf("enter 3 numbers : ");
     scanf("%d %d %d",&a,&b,&c);
-    if (a>b && a>c){
+    if (a>=b && a>=c){
         printf("%d is the largest number",a);
-    }if (b>a && b>c){
+    } else if (b>=a && b>=c){
         printf("%d is the largest number",b);
-    }if (c>a && c>b){
+    } else {
         printf("%d is the largest number",c);
     }
-        return 0;
-    }
+    return 0;
+}
