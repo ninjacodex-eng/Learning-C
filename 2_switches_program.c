117b13c703c464717bee15b91f3374a08926066d
Aditya <adityaboura09@gmail.com>
2026-06-15 19:28:00 +0530

    Add switches to print values of i and j

---
+++ 2_switches_program.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+int i=10;
+int j=20;
+switch(i){
+case 10:
+    printf("this is value of i : %d\n",i);
+    break;
+}
+switch(j){
+case 20:
+    printf("this is value of j : %d\n",j);
+    break;
+}
+return 0;
+}
