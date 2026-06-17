417175c204015446eb926934cfad8ca56ec97600
Aditya <adityaboura09@gmail.com>
2026-06-17 19:57:45 +0530

    Modify multiplication table output format

--- any_input_table.c
+++ any_input_table.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main(){
-    int i=1,num=0;
+    int i=1;
+    int a=0;
     printf("Enter a no : ");
-    scanf("%d",&num);
+    scanf("%d",&a);
     do
     {
-        printf("%d\n",num*i);
+        printf("%d X %d = %d\n",a,i,a*i);
         i++;
     }
-   while(i<=10);
-   return 0;
+    while(i<=10);
+    return 0;
 }
