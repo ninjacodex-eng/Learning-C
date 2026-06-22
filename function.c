be8b7c01872dd64a4a3b4d9a921e750f29a673a2
Aditya <adityaboura09@gmail.com>
2026-06-22 18:56:42 +0530

    Refactor main function and clean up comments

--- function.c
+++ function.c
@@ -1,19 +1,19 @@
 #include <stdio.h>  
   
-// Function Declaration  
+
 int multiply(int, int);  
   
-int main() {   //main function  
+int main() {   
     int result;  
   
-    // Calling the add function  
+  
     result = multiply(4, 5);  
   
     printf("The Multiplication of Numbers is: %d\n", result);  
     return 0;  
 }  
   
-// Function Definition  
+
 int multiply(int a, int b) {  
     return a * b;  
 }  
