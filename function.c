f3e9f71d085839025445902051cd036ca3065f9b
Aditya <adityaboura09@gmail.com>
2026-06-22 18:55:45 +0530

    Add multiplication function and main program

--- function.c
+++ function.c
@@ -1,1 +1,19 @@
-Error
+#include <stdio.h>  
+  
+// Function Declaration  
+int multiply(int, int);  
+  
+int main() {   //main function  
+    int result;  
+  
+    // Calling the add function  
+    result = multiply(4, 5);  
+  
+    printf("The Multiplication of Numbers is: %d\n", result);  
+    return 0;  
+}  
+  
+// Function Definition  
+int multiply(int a, int b) {  
+    return a * b;  
+}  
