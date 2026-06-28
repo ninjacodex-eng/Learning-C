530707b30898fc20ad1e5daaaf0a2fa9568c9aec
Aditya <adityaboura09@gmail.com>
2026-06-28 11:44:34 +0530

    Correct 2D array syntax and loop structure
    Fix syntax errors and print elements of a 2D array.

--- 05-Arrays/2d_arrays_loop.c
+++ 05-Arrays/2d_arrays_loop.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main(){ 
+  int num[3][3]={
+    {3,4,5},
+    {7,8,9},
+    {3,6,8}
+  };
+  for(int i=0; i<3; i++){
+      for(int j=0; j<3; j++){
+          printf(" %d",num[i][j]);
+      }
+       printf("\n");
+  }
+return 0;
+}
