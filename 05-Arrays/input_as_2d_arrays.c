f4393c9d84943a4e29d12eeff491b55fcf3f4345
Aditya <adityaboura09@gmail.com>
2026-06-28 12:27:07 +0530

    Add input and output for 2D array in C
    Initialize a 3x3 array and allow user input for its values. Print the array after input.

---
+++ 05-Arrays/input_as_2d_arrays.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int num[3][3]={
+      {1,2,3},
+      {4,5,6},
+      {7,8,9}
+    };
+    printf("enter 9 values : ");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+          
+            scanf("%d",&num[i][j]);
+        }
+    }
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            printf(" %d",num[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
